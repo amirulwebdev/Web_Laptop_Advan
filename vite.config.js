c8985092fe2de0ec39b9fff351d8c73bbe798b0c
Muhammadamirul0 <muhammadamirul05.am@gmail.com>
2025-04-24 23:26:10 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Web_Laptop_Advan/',
   plugins: [
     react(),
     tailwindcss(),
